a8a4632a4780ac74196078d821b9a9a34a17327a
Ayush khairnar <141556340+TechNinjaAyush@users.noreply.github.com>
2025-02-15 11:30:02 +0530

    Added frontend folder

---
+++ frontend/vite-project/src/Auth/Register.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const  Register = ()=>{
+    const[]
+
+
+
+    return(
+
+
+        <div>
+    
+        
+
+        </div>
+    )
+}
+
+export   default  Register; 
+
